77a594156d25a29f0d8520e12d7120e82e8078f5
Drishti <drishtigoel083@gmail.com>
2026-01-17 15:26:43 +0530

    Import issue resolve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router,Routes,Route } from "react-router-dom"
-import Home from "./pages/Home.jsx"
+import Home from "./pages/Home"
 import UploadNotesPage from "./pages/upload.jsx"
 import Register from "./pages/Register.jsx"
 import LoginPage from "./pages/Login.jsx"
